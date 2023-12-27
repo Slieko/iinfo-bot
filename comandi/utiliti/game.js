163f20fcdab9873d97ab0b07948d8597e45336af
Slieko <bdimyt@gmail.com>
2023-12-27 15:31:00 +0300

    cooldown (idk), giverole

--- comandi/utiliti/game.js
+++ comandi/utiliti/game.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
+	cooldown: 10,
 	data: new SlashCommandBuilder()
 		.setName('game')
 		.setDescription('a simple game'),
